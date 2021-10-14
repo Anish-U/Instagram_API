0e7263e79378637d246c0a7c1dd795ce753d4dd9
Anish <ummenthala.anish@gmail.com>
2021-10-14 17:23:37 +0530

    unit test for createUser handler added

--- server_test.go
+++ server_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -28,7 +29,9 @@ func TestGetUserHandler(t *testing.T) {
 	// Serving a test HTTP to handle test request
 	handler.ServeHTTP(rec, r)
 
+	// Flag variable for errors
 	errors := false
+
 	// Checking for status code difference
 	if status := rec.Code; status != http.StatusOK {
 		t.Error("Handler returned wrong status code: \nreceived {} \nexpected {} \n",
@@ -36,16 +39,73 @@ func TestGetUserHandler(t *testing.T) {
 		errors = true
 	}
 
-	// Checking for body content difference
+	// Checking for response body difference
 	if strings.TrimSpace(rec.Body.String()) != strings.TrimSpace(expectedUser) {
-		t.Error("Handler returned unexpected body content: \nreceived {} \nexpected {} \n",
+		t.Error("Handler returned unexpected respose body: \nreceived {} \nexpected {} \n",
 			strings.TrimSpace(rec.Body.String()),
-			strings.Trim(expectedUser, "\\s"))
+			strings.TrimSpace(expectedUser))
 		errors = true
 	}
 
 	// If no errors log Success Status
 	if !errors {
-		log.Println(" GET /users/ - getUsers PASSED ✅")
+		log.Println(" GET /users/{id} - getUsers PASSED ✅")
+	}
+}
+
+func TestCreateUserHandler(t *testing.T) {
+
+	// String format JSON of expected test user
+	json := `{
+						"Name":"testUser1",
+						"Email":"testUser1@mail.com",
+						"Password":"testPassword1"
+					}`
+
+	// String converted to bytes array
+	jsonBytes := []byte(json)
+
+	// Created a new HTTP request to post test user
+	r, err := http.NewRequest("POST", "/users/", bytes.NewBuffer(jsonBytes))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Setting headers for HTTP request
+	r.Header.Set("Content-Type", "application/json")
+
+	// HTTPtest recorder
+	rec := httptest.NewRecorder()
+
+	// Retrieving handler to handle users requests
+	handler := http.HandlerFunc(userHandler)
+
+	// String format JSON of expected body after successful creation of user
+	expectedBody := `{"success":"User Added successful"}`
+
+	// Serving a test HTTP to handle test request
+	handler.ServeHTTP(rec, r)
+
+	// Flag variable for errors
+	errors := false
+
+	// Checking for status code difference
+	if status := rec.Code; status != http.StatusOK {
+		t.Error("Handler returned wrong status code: \nreceived {} \nexpected {} \n",
+			status, http.StatusOK)
+		errors = true
+	}
+
+	// Checking for response body difference
+	if strings.TrimSpace(rec.Body.String()) != strings.TrimSpace(expectedBody) {
+		t.Error("Handler returned unexpected response body: \nreceived {} \nexpected {} \n",
+			strings.TrimSpace(rec.Body.String()),
+			strings.TrimSpace(expectedBody))
+		errors = true
+	}
+
+	// If no errors log Success Status
+	if !errors {
+		log.Println(" POST /users/ - createUser PASSED ✅")
 	}
 }
